4628eb35fd7b2935a81c7f3af43bcbb30d947baa
devmirza <mamasaidovmirzabek@gmail.com>
2023-02-15 14:52:25 +0500

    for deployment

--- src/main/java/uz/unicon/charityproject/CharityProjectApplication.java
+++ src/main/java/uz/unicon/charityproject/CharityProjectApplication.java
@@ -2,12 +2,31 @@ package uz.unicon.charityproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class CharityProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CharityProjectApplication.class, args);
+    }
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        org.springframework.web.cors.CorsConfiguration corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("http://localhost:3000/");
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD","PATCH"));
+        corsConfiguration.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
     }
 
 }
